42de4df10ad5202a5637d40971fe72da736d6719
Arthur Verdon <arthur.verdon@liip.ch>
2025-10-30 21:40:53 +0100

    Solarized theme, sane simple config

--- init.lua
+++ init.lua
@@ -1,13 +1,28 @@
-local nvimConfigPath = '~/.config/nvim/'
+require("config.lazy")
 
-vim.cmd('source ' .. nvimConfigPath .. 'lua/core/plugins.lua')
+vim.opt.guicursor = ""
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = false
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.cmdheight = 0
+vim.opt.colorcolumn = '100'
+vim.opt.wildmode = 'longest:full,full'
+vim.o.bg='light'
+vim.cmd.colorscheme 'gruvbox'
 
--- Autoload plugins configuration/setup
-local paths = vim.split(vim.fn.glob('~/.config/nvim/lua/plugins/**/*.lua'), '\n')
-for _, file in pairs(paths) do
-  vim.cmd('source ' .. file)
-end
-
-vim.cmd('source ' .. nvimConfigPath .. 'lua/core/globals.lua')
-vim.cmd('source ' .. nvimConfigPath .. 'lua/core/mappings.lua')
-
+-- Map
+vim.keymap.set('n', '<leader><leader>', require('telescope.builtin').builtin, { desc = 'Display Telescope' })
+vim.keymap.set('n', '<C-N>', require('telescope.builtin').find_files, { desc = 'Search Files' })
+vim.keymap.set('n', '<C-f>', require('telescope.builtin').live_grep, { desc = 'Search string in current workspace' })
--- lua/plugins/marks.lua
+++
@@ -1,9 +0,0 @@
-require 'marks'.setup {
-  refresh_interval = 250,
-  -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
-  -- marks, and bookmarks.
-  -- can be either a table with all/none of the keys, or a single number, in which case
-  -- the priority applies to all marks.
-  -- default 10.
-  sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
-}
